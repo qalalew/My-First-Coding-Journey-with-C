bd1206e9dbe4491c03156bd049d4fa36ea57243f
Kalalew Terefe <kalalewtere@gmail.com>
2025-10-12 17:52:47 +0300

    Update star-pyramid.py

--- star-pyramid.py
+++ star-pyramid.py
@@ -2,9 +2,7 @@ def centered_pyramid(h):
     for i in range(1, h+1):
         spaces = h - i
         stars = 2 * i - 1
-        # Print spaces followed by stars
         print(" " * spaces + "*" * stars)
 
-# Get user input for height
 height = int(input("Enter the height of the pyramid: "))
 centered_pyramid(height)
