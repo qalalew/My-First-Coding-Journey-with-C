cb9c887c0695f5e260222267eac56bea6e16849f
Kalalew Terefe <kalalewtere@gmail.com>
2025-10-17 11:02:51 +0300

    feat: calculate and print average marks for 2 subjects

--- star-pyramid.py
+++
@@ -1,8 +0,0 @@
-def centered_pyramid(h):
-    for i in range(1, h+1):
-        spaces = h - i
-        stars = 2 * i - 1
-        print(" " * spaces + "*" * stars)
- 
-height = int(input("please can you Enter the height of the pyramid?: "))
-centered_pyramid(height)
